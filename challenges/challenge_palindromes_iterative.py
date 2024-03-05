c194056a99d0a1c67017970a3ff9a79867f3832e
ElisaBorba <elisaborbaa@gmail.com>
2024-03-05 02:13:25 -0300

    req 6

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,4 +1,14 @@
 def is_palindrome_iterative(word):
+
+    if word == "":
+        return False
+
     for i in range(len(word)):
         if word[i] != word[len(word) - 1 - i]:
             return False
+    return True
+
+
+print(is_palindrome_iterative("racecar"))
+print(is_palindrome_iterative("hello"))
+print(is_palindrome_iterative(""))
