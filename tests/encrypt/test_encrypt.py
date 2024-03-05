9d8813ca0fc961589dd8e40baea67e7e09b2621a
ElisaBorba <elisaborbaa@gmail.com>
2024-03-05 01:31:19 -0300

    tests encrypted message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,15 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+
+    assert encrypt_message("Testando", 3) == "seT_odnat"
+    assert encrypt_message("Testando", 4) == "odna_tseT"
+    assert encrypt_message("Testando", 9) == "odnatseT"
+
+    with pytest.raises(TypeError):
+        encrypt_message(9, 9)
+
+    with pytest.raises(TypeError):
+        encrypt_message(9, "9")
